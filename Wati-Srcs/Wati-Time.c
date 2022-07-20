e0a280ddf211077c3006b6427bacee90ab01dc5e
Wati-Theo42 <schlegel.theo@outlook.fr>
2022-07-20 23:22:43 +0200

    get time difference now OP

--- Wati-Srcs/Wati-Time.c
+++ Wati-Srcs/Wati-Time.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:59:41 by tschlege          #+#    #+#             */
-/*   Updated: 2022/07/20 22:29:15 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2022/07/20 23:22:31 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ unsigned int	get_time_difference(struct timeval old_time)
 {
 	struct timeval	current_time;
 	unsigned int	time_current;
-	unsigned int	time_j;
+	unsigned int	time_old;
 
 	gettimeofday(&current_time, NULL);
-	time_i = current_time.tv_sec * (unsigned int)1000 + current_time.tv_usec
+	time_current = current_time.tv_sec * (unsigned int)1000 + current_time.tv_usec
 		/ 1000;
-	time_j = old_time.tv_sec * (unsigned int)1000 + old_time.tv_usec / 1000;
-	return (time_i - time_j);
+	time_old = old_time.tv_sec * (unsigned int)1000 + old_time.tv_usec / 1000;
+	return (time_current - time_old);
 }
